df130e16f634abc9bdbf575799e019ab799d073d
Lollyde <github@lollyde.online>
2025-01-22 21:17:04 +0100

    add rollator prototype

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-@export var turn_degrees := 15
-@export var offset_amount := 20
-@export var speed := 400
+@export var walking_speed := 400
+@export var crutches_turn_degrees := 15
+@export var crutches_offset_amount := 20
+@export var rollator_speed := 300
+@export var rollator_turn_degrees := 15
+@export var rollator_offset_amount := 20
 
 enum move_state {WALKING, CRUTCHES, ROLLATOR, MANUAL_WHEELCHAIR, POWER_WHEELCHAIR}
 
@@ -19,7 +22,9 @@ func _physics_process(delta: float) -> void:
 		move_state.CRUTCHES:
 			crutches_movement(delta)
 		move_state.ROLLATOR:
-			pass
+			self.rotate(PI / -2) #workaround, dont change
+			rollator_movement(delta)
+			self.rotate(PI / 2) #workaround, dont change
 		move_state.MANUAL_WHEELCHAIR:
 			pass
 		move_state.POWER_WHEELCHAIR:
@@ -38,7 +43,7 @@ func walking_movement(delta: float):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * walking_speed
 		self.rotation = velocity.rotated(PI/2).angle()
 	
 	position += velocity * delta
@@ -54,26 +59,64 @@ func crutches_movement(_delta: float):
 	
 	self.velocity = Vector2.ZERO
 	if Input.is_action_just_pressed("move_o"):
-		self.move_left()
-		self.rotation_degrees -= turn_degrees
-		self.move_right()
+		rotate_with_offset(-crutches_turn_degrees, false, crutches_offset_amount)
 	elif Input.is_action_just_pressed("move_q"):
-		self.move_right()
-		self.rotation_degrees += turn_degrees
-		self.move_left()
+		rotate_with_offset(crutches_turn_degrees, true, crutches_offset_amount)
 	elif Input.is_action_just_pressed("move_l"):
-		self.move_left()
-		self.rotation_degrees += turn_degrees
-		self.move_right()
+		rotate_with_offset(crutches_turn_degrees, false, crutches_offset_amount)
 	elif Input.is_action_just_pressed("move_a"):
-		self.move_right()
-		self.rotation_degrees -= turn_degrees
-		self.move_left()
+		rotate_with_offset(-crutches_turn_degrees, true, crutches_offset_amount)
 
-func move_left():
+func rollator_movement(_delta: float):
+	#self.velocity = Vector2.ZERO
+	# gotta have an input for *both* sides!!
+	if Input.is_action_just_pressed("move_q"):
+		if Input.is_action_pressed("move_o"):
+			self.velocity = Vector2.from_angle(self.rotation).normalized() * rollator_speed
+			pass
+		elif Input.is_action_pressed("move_l"):
+			rotate_with_offset(rollator_turn_degrees, true, rollator_offset_amount)
+			pass
+	elif Input.is_action_just_pressed("move_a"):
+		if Input.is_action_pressed("move_o"):
+			rotate_with_offset(-rollator_turn_degrees, true, rollator_offset_amount)
+			pass
+		elif Input.is_action_pressed("move_l"):
+			self.velocity = Vector2.from_angle(self.rotation).normalized() * rollator_speed * -1
+			pass
+	elif Input.is_action_just_pressed("move_o"):
+		if Input.is_action_pressed("move_q"):
+			self.velocity = Vector2.from_angle(self.rotation).normalized() * rollator_speed
+			pass
+		elif Input.is_action_pressed("move_a"):
+			rotate_with_offset(-rollator_turn_degrees, false, rollator_offset_amount)
+			pass
+	elif Input.is_action_just_pressed("move_l"):
+		if Input.is_action_pressed("move_q"):
+			rotate_with_offset(rollator_turn_degrees, false, rollator_offset_amount)
+			pass
+		elif Input.is_action_pressed("move_a"):
+			self.velocity = Vector2.from_angle(self.rotation).normalized() * rollator_speed * -1
+			pass
+	
+	self.velocity *= 0.8
+	if self.velocity.length() < 30:
+		self.velocity = Vector2.ZERO
+
+func rotate_with_offset(turn_degrees: float, offset_right: bool, offset: float):
+	if offset_right:
+		self.move_right(offset)
+		self.rotation_degrees += turn_degrees
+		self.move_left(offset)
+	else:
+		self.move_left(offset)
+		self.rotation_degrees += turn_degrees
+		self.move_right(offset)
+
+func move_left(offset_amount: float):
 	var left_vec = Vector2(-1, 0).rotated(self.rotation) * offset_amount
 	self.translate(left_vec)
 	
-func move_right():
+func move_right(offset_amount: float):
 	var right_vec = Vector2(1, 0).rotated(self.rotation) * offset_amount
 	self.translate(right_vec)

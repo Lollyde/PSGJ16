dc60fa9a263306473c39a3a954b6cc06bdec0aa6
Lollyde <github@lollyde.online>
2025-01-30 16:51:09 +0100

    redo wheelchair movement, fixes #2

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -5,18 +5,20 @@ extends CharacterBody2D
 @export var crutches_offset_amount := 20
 @export var rollator_speed := 200
 @export var rollator_turn := 45
-@export var manual_wheelchair_speed := 800
+@export var manual_wheelchair_speed := 400
+@export var manual_speed_multiplier := 0.0
 @export var manual_wheelchair_turn_degrees := 10
+@export var manual_wheelchair_turn_request := 0.0
 @export var manual_wheelchair_offset_amount := 20
 @export var power_wheelchair_speed := 300
-@export var power_wheelchair_turn = 90
+@export var power_wheelchair_turn := 90
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 enum move_state {WALKING, CRUTCHES, ROLLATOR, MANUAL_WHEELCHAIR, POWER_WHEELCHAIR}
 
 var current_mode: move_state = move_state.WALKING
-
+var last_manual_speed_multiplier := 0.0
 
 func _debug_change_mode(index: int):
 	current_mode = index as move_state
@@ -119,7 +121,44 @@ func rollator_movement(delta: float):
 	
 	position += vel * delta
 
-func manual_wheelchair_movement(_delta: float):
+func reset_manual():
+	sprite.play("anim_manual_idle")
+
+func manual_wheelchair_movement(delta: float):
+	# rewrite time!
+	var req_forward = Input.is_action_pressed("wheelchair_left_forward") && Input.is_action_pressed("wheelchair_right_forward")
+	var req_backward = Input.is_action_pressed("wheelchair_left_backward") && Input.is_action_pressed("wheelchair_right_backward")
+	var req_turnright = Input.is_action_pressed("wheelchair_left_forward") && Input.is_action_pressed("wheelchair_right_backward")
+	var req_turnleft = Input.is_action_pressed("wheelchair_left_backward") && Input.is_action_pressed("wheelchair_right_forward")
+	var newInput = Input.is_action_just_pressed("wheelchair_left_forward") ||  Input.is_action_just_pressed("wheelchair_right_forward") || Input.is_action_just_pressed("wheelchair_left_backward") || Input.is_action_just_pressed("wheelchair_right_backward")
+	
+	if newInput:
+		if req_forward:
+			print("manual_forward")
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play_with_capture("manual_forward")
+		elif req_backward:
+			print("manual_backward")
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play_with_capture("manual_backward")
+		elif req_turnright:
+			print("manual_right")
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play_with_capture("manual_right")
+		elif req_turnleft:
+			print("manual_left")
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play_with_capture("manual_left")
+	else:
+		if !$AnimationPlayer.is_playing():
+			$AnimationPlayer.play("manual_idle")
+	self.rotation_degrees += manual_wheelchair_turn_request * delta * manual_wheelchair_turn_degrees
+	self.position += Vector2.RIGHT.rotated(self.rotation).normalized() * delta * manual_speed_multiplier * manual_wheelchair_speed
+		
+	#if manual_speed_multiplier != 0:
+		# print("requested speed multiplier: " + str(manual_speed_curve.sample(manual_speed_progress)))
+	return
+	
 	#self.velocity = Vector2.ZERO
 	# gotta have an input for *both* sides!!
 	var rot = 0 # tracking for animation purposes

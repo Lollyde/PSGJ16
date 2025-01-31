524859108c860d051b6b7f53158ca6a11f3d6baa
Lollyde <github@lollyde.online>
2025-01-31 14:08:11 +0100

    fix mode changer

--- assets/scripts/game_manager.gd
+++ assets/scripts/game_manager.gd
@@ -46,31 +46,28 @@ func nextlevel():
 	level_3.visible = false
 	level_4.visible = false
 	level_5.visible = false
+	player.change_mode(current_level)
 	match current_level:
 		1:
 			current_level = 2
 			level_2.visible = true
 			level_2.set_process(true)
 			player.global_position = level_2.get_spawn()
-			player.change_mode(current_level - 2)
 		2:
 			current_level = 3
 			level_3.visible = true
 			level_3.set_process(true)
 			player.global_position = level_3.get_spawn()
-			player.change_mode(current_level - 2)
 		3:
 			current_level = 4
 			level_4.visible = true
 			level_4.set_process(true)
 			player.global_position = level_4.get_spawn()
-			player.change_mode(current_level - 2)
 		4:
 			current_level = 5
 			level_5.visible = true
 			level_5.set_process(true)
 			player.global_position = level_5.get_spawn()
-			player.change_mode(current_level - 2)
 		5:
 			# congrats screen
 			pass

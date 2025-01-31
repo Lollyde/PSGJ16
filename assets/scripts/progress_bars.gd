4d71bc24717b9663fe0c7a25f24c884709805aa6
tinytyra <39837549+tinytyra@users.noreply.github.com>
2025-01-31 12:06:42 +0100

    there are levels

--- assets/scripts/progress_bars.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-func update_happy_bar():
-	$Bars/ProgressBars/happy_bar.set_value_no_signal(happy_points)
-	
-	
-func update_energy_bar():
-	$Bars/ProgressBars/energy_bar.set_value_no_signal(energy_points)
